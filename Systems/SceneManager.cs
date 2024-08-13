8d1da227b806341ef66c4610c291b8c64a9e9bbc
JANC_PC\czech <czechumennakompie@gmail.com>
2024-08-13 13:09:40 +0200

    Scene manager class refactor.

--- Systems/SceneManager.cs
+++ Systems/SceneManager.cs
@@ -63,6 +63,166 @@ namespace LF2Clone.Systems
             }
         }
 
+        #region Scene loading
+
+
+        // it should deserialize a scene, add it to _loadedScenesList
+        // returns true if loading was successful, false otherwise
+        public bool TryLoadScene(string name)
+        {
+            if (string.IsNullOrEmpty(_scenesFolderPath))
+            {
+                _logger.LogError("Scene folder path not found.");
+                return false;
+            }
+
+            try
+            {
+                if (_loadedScenesList.Select(x => x._name).Contains(name))
+                {
+                    _logger.LogError(string.Format("Scene with name {0} is already loaded.", name));
+                    return false;
+                }
+
+                var scene = DeserializeScene(_scenesFolderPath, name);
+                if (scene != null)
+                {
+                    _loadedScenesList.Add(scene);
+                    _logger.LogDebug(string.Format("Scene {0} loaded successfuly.", name));
+                    return true;
+                }
+                _logger.LogError(string.Format("Scene {0} was not loaded successfuly.", name));
+                return false;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("Exception while opening the file to deserialize. Exception: {0}", ex.Message));
+                return false;
+            }
+        }
+
+        // serialize the scene, remove it from _loadedScenesList
+        // returns true if unloading was successful, false otherwise
+        public bool TryUnloadScene(string name)
+        {
+            if (string.IsNullOrEmpty(_scenesFolderPath))
+            {
+                _logger.LogError("Scene folder path not found.");
+                return false;
+            }
+
+            Scene scene;
+
+            try
+            {
+                scene = _loadedScenesList.First(x => x._name == name);
+                _logger.LogDebug(string.Format("Found scene with the name {0}", name));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("A scene with name {0}, could not be found. Exception: {1}", name, ex.Message));
+                return false;
+            }
+
+            try
+            {
+                if (!SerializeScene(_scenesFolderPath, scene, true))
+                {
+                    _logger.LogError("Could not serialize a scene. Check if the provided path is correct.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("Could not serialize a scene. Exception: {0}", ex.Message));
+                return false;
+            }
+
+            // is this should be set as null or return false?
+            if (_currentScene._id == scene._id)
+            {
+                _currentScene = null;
+            }
+
+            _loadedScenesList.Remove(scene);
+            _logger.LogInfo(string.Format("Scene {0} unloaded.", name));
+            return true;
+        }
+
+        // returns true if the scene was serialized successfuly, otherwise false
+        private bool SerializeScene(string path, Scene scene, bool overwrite = false)
+        {
+            try
+            {
+                var attributes = File.GetAttributes(path);
+                //LFsc - LF scene
+                var filename = string.Format("{0}\\{1}.LFsc.json", path, scene._name);
+
+                switch (attributes)
+                {
+                    case FileAttributes.Directory:
+                        if (Directory.Exists(path))
+                        {
+                            if (File.Exists(filename) && overwrite || !File.Exists(filename))
+                            {
+                                using (var sw = new StreamWriter(filename))
+                                {
+                                    sw.Write(JsonConvert.SerializeObject(scene, Formatting.Indented));
+                                    sw.Dispose();
+                                }
+                                _logger.LogDebug(string.Format("File written as {0}", filename));
+                                return true;
+                            }
+                        }
+                        _logger.LogError("Directory not found.");
+                        return false;
+                    default:
+                        if (File.Exists(path) || File.Exists(filename))
+                        {
+                            _logger.LogWarning("There is already a file with a name like this.");
+                            if (overwrite)
+                            {
+                                using (var sw = new StreamWriter(filename))
+                                {
+                                    sw.Write(JsonConvert.SerializeObject(scene, Formatting.Indented));
+                                }
+                                _logger.LogDebug(string.Format("File written as {0}", filename));
+                            }
+                            return true;
+                        }
+                        return false;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private Scene? DeserializeScene(string path, string name)
+        {
+            var filename = string.Format("{0}\\{1}.LFsc.json", path, name);
+            Scene? scene = null;
+
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    using (var sr = new StreamReader(filename))
+                    {
+                        var read = sr.ReadToEnd();
+                        scene = JsonConvert.DeserializeObject<Scene>(read);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return scene;
+        }
+
         public void ShowLoadedScenes()
         {
             foreach (var scene in _loadedScenesList)
@@ -75,6 +235,10 @@ namespace LF2Clone.Systems
                 _logger.LogInfo(string.Format("Currently loaded scene: {0}", _currentScene._name));
             }
         }
+
+        #endregion
+
+        #region Scene switching
 
         public bool TrySetCurrentScene(int id)
         {
@@ -143,162 +307,7 @@ namespace LF2Clone.Systems
             return true;
         }
 
-        // it should deserialize a scene, add it to _loadedScenesList
-        // returns true if loading was successful, false otherwise
-        public bool TryLoadScene(string name)
-        {
-            if (string.IsNullOrEmpty(_scenesFolderPath))
-            {
-                _logger.LogError("Scene folder path not found.");
-                return false;
-            }
-
-            try
-            {
-                if (_loadedScenesList.Select(x => x._name).Contains(name))
-                {
-                    _logger.LogError(string.Format("Scene with name {0} is already loaded.", name));
-                    return false;
-                }
-
-                var scene = DeserializeScene(_scenesFolderPath, name);
-                if (scene != null)
-                {
-                    _loadedScenesList.Add(scene);
-                    _logger.LogDebug(string.Format("Scene {0} loaded successfuly.", name));
-                    return true;
-                }
-                _logger.LogError(string.Format("Scene {0} was not loaded successfuly.", name));
-                return false;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(string.Format("Exception while opening the file to deserialize. Exception: {0}", ex.Message));
-                return false;
-            }
-        }
-
-        // serialize the scene, remove it from _loadedScenesList
-        // returns true if unloading was successful, false otherwise
-        public bool TryUnloadScene(string name)
-        {
-            if(string.IsNullOrEmpty(_scenesFolderPath))
-            {
-                _logger.LogError("Scene folder path not found.");
-                return false;
-            }
-
-            Scene scene;
-
-            try
-            {
-                scene = _loadedScenesList.First(x => x._name == name);
-                _logger.LogDebug(string.Format("Found scene with the name {0}", name));
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(string.Format("A scene with name {0}, could not be found. Exception: {1}", name, ex.Message));
-                return false;
-            }
-
-            try
-            {
-                if (!SerializeScene(_scenesFolderPath, scene, true))
-                {
-                    _logger.LogError("Could not serialize a scene. Check if the provided path is correct.");
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(string.Format("Could not serialize a scene. Exception: {0}", ex.Message));
-                return false;
-            }
-
-            // is this should be set as null or return false?
-            if (_currentScene._id == scene._id)
-            {
-                _currentScene = null;
-            }
-
-            _loadedScenesList.Remove(scene);
-            _logger.LogInfo(string.Format("Scene {0} unloaded.", name));
-            return true;
-        }
-
-        // returns true if the scene was serialized successfuly, otherwise false
-        private bool SerializeScene(string path, Scene scene, bool overwrite = false)
-        {
-            try
-            {
-                var attributes = File.GetAttributes(path);
-                //LFsc - LF scene
-                var filename = string.Format("{0}\\{1}.LFsc.json", path, scene._name);
-
-                switch (attributes)
-                {
-                    case FileAttributes.Directory:
-                        if (Directory.Exists(path))
-                        {
-                            if(File.Exists(filename) && overwrite || !File.Exists(filename))
-                            {
-                                using (var sw = new StreamWriter(filename))
-                                {
-                                    sw.Write(JsonConvert.SerializeObject(scene, Formatting.Indented));
-                                    sw.Dispose();
-                                }
-                                _logger.LogDebug(string.Format("File written as {0}", filename));
-                                return true;
-                            }
-                        }
-                        _logger.LogError("Directory not found.");
-                        return false;
-                    default:
-                        if (File.Exists(path) || File.Exists(filename))
-                        {
-                            _logger.LogWarning("There is already a file with a name like this.");
-                            if (overwrite)
-                            {
-                                using (var sw = new StreamWriter(filename))
-                                {
-                                    sw.Write(JsonConvert.SerializeObject(scene, Formatting.Indented));
-                                }
-                                _logger.LogDebug(string.Format("File written as {0}", filename));
-                            }
-                            return true;
-                        }
-                        return false;
-                }
-            }
-            catch
-            {
-                throw;
-            }
-        }
-
-        public Scene? DeserializeScene(string path, string name)
-        {
-            var filename = string.Format("{0}\\{1}.LFsc.json", path, name);
-            Scene? scene = null;
-
-            try
-            {
-                if (File.Exists(filename))
-                {
-                    using (var sr = new StreamReader(filename))
-                    {
-                        var read = sr.ReadToEnd();
-                        scene = JsonConvert.DeserializeObject<Scene>(read);
-                    }
-                }
-            }
-            catch
-            {
-                throw;
-            }
-
-            return scene;
-        }
+        #endregion
 
     }
 }
