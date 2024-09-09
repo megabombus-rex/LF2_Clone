4eddb2b8bc8b85f9765ccc513320f98aef838c1f
megabombus-rex <czechumennakompie@gmail.com>
2024-09-09 18:17:54 +0200

    Updated base component logic.

--- Base/Component.cs
+++ Base/Component.cs
@@ -1,21 +1,28 @@
-﻿using Raylib_cs;
+﻿using LF2Clone.Base.Interfaces;
+using Raylib_cs;
 
 namespace LF2Clone.Base
 {
     // Components should work as attributes for the nodes, so each node has its own set of components
-    public class Component
+    public class Component : IDrawable
     {
         public Transform _transform;
         public bool _isDrawable;
         public bool _isActive;
+        public string _name;
+        public int _id;
                 
         public Component()
         {
+            _name = "comp";
         }
 
         public virtual void Draw()
         {
-
+            if (!_isDrawable)
+            {
+                throw new InvalidOperationException(string.Format("Component {0} is not drawable.", _name));
+            }  
         }
 
         public virtual void Awake()
