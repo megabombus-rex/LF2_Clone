47283a53e7401111af3ab5c61ff5521312b0b653
JANC_PC\czech <czechumennakompie@gmail.com>
2024-09-11 23:08:55 +0200

    Added PositioningHelper and base Label class.

---
+++ Base/Helpers/PositioningHelper.cs
@@ -0,0 +1,13 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace LF2Clone.Base.Helpers
+{
+    public static class PositioningHelper
+    {
+        public static Vector2 GetCenterOfRectangle(ref Rectangle rectangle) 
+        {
+            return new Vector2((rectangle.Position.X + rectangle.Width) / 2, (rectangle.Position.Y + rectangle.Height) / 2);
+        }
+    }
+}
---
+++ UI/Label.cs
@@ -0,0 +1,38 @@
+﻿using LF2Clone.Base;
+using LF2Clone.Base.Helpers;
+using Raylib_cs;
+using System.Numerics;
+
+namespace LF2Clone.UI
+{
+    public class Label : Component
+    {
+        private string _text;
+        private int _fontSize;
+        private Rectangle _backgroundRec;
+        private Texture2D _backgroundTexture;
+
+        public Label(string text, int fontSize, int sizeX, int sizeY, Texture2D backText, 
+            bool isActive, string name, int id) 
+            : base(true, isActive, name, id)
+        {
+            _text = text;
+            _fontSize = fontSize;
+            _backgroundRec = new Rectangle()
+            {
+                Height = sizeY,
+                Width = sizeX,
+                Position = new Vector2(0, 0)
+            };
+            _backgroundTexture = backText;
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            Raylib.DrawTexturePro(_backgroundTexture, _backgroundRec, _backgroundRec, _backgroundRec.Position, 0.0f, Color.Blank);
+            var center = PositioningHelper.GetCenterOfRectangle(ref _backgroundRec);
+            Raylib.DrawText(_text, (int)center.X, (int)center.Y, _fontSize, Color.Black);
+        }
+    }
+}
