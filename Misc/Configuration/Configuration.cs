2b218098eace35e455a1ac6622c53311ec55fb42
megabombus-rex <czechumennakompie@gmail.com>
2024-09-16 14:02:39 +0200

    Added days of life for logs config.

--- Misc/Configuration/Configuration.cs
+++ Misc/Configuration/Configuration.cs
@@ -3,6 +3,7 @@
     public class Configuration
     {
         public Dictionary<string, Logging> LoggerConfigs;
+        public int LoggingDaysOfLife; 
         public string LoggingFilePath; // one file for all AllLog-date.log
         public string Version;
         public Resolution StartingScreenResolution;
