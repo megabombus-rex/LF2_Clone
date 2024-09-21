54027817800691e284d4014279ecd5e81d3fa5ee
JANC_PC\czech <czechumennakompie@gmail.com>
2024-09-21 11:17:09 +0200

    Added SoundManager system class.

---
+++ Systems/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using LF2Clone.Base;
+using LF2Clone.Misc.Logger;
+using Raylib_cs;
+
+namespace LF2Clone.Systems
+{
+    public sealed class SoundManager : System<SoundManager>
+    {
+        public SoundManager(ILogger logger) : base(logger)
+        {
+        }
+
+        private Music _musicPlaying;
+        private bool _isMusicPlaying;
+
+        public event PlaySound Play;
+        public delegate void PlaySound(object sender, EventArgs e);
+    }
+}
