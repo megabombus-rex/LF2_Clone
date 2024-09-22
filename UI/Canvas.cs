b14d3dc08d965f541130bbaf358b3b43d6431609
megabombus-rex <czechumennakompie@gmail.com>
2024-09-22 14:15:23 +0200

    Added canvas UIComponent.

---
+++ UI/Canvas.cs
@@ -0,0 +1,13 @@
+﻿using LF2Clone.Base;
+using Raylib_cs;
+
+namespace LF2Clone.UI
+{
+    public class Canvas : UIComponent
+    {
+        public Canvas(float rotation, Transform transform, bool isActive, string name, int id) 
+            : base(rotation, transform, isActive, name, id)
+        {
+        }
+    }
+}
