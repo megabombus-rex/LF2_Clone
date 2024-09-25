a07427aeb120578118c31391b388cbbdeb1f1056
megabombus-rex <czechumennakompie@gmail.com>
2024-09-25 17:59:05 +0200

    Extended logger to a non-typed one. Added a wrapper for future raylib logging.

---
+++ Helpers/UnmanagedCodeHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace LF2Clone.Helpers
+{
+    public static class UnmanagedCodeHelper
+    {
+        public static unsafe sbyte* StringToSbyte(string text, Encoding e)
+        {
+            byte[] bytes = e.GetBytes(text);
+
+            unsafe
+            {
+                fixed (byte* ptr = bytes)
+                {
+                    return (sbyte*)ptr;
+                }
+            }
+        }
+    }
+}
---
+++ Misc/Logger/RaylibLoggerWrapper.cs
@@ -0,0 +1,40 @@
+﻿namespace LF2Clone.Misc.Logger
+{
+    public class RaylibLoggerWrapper
+    {
+        public RaylibLoggerWrapper(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        private ILogger _logger;
+
+        public void LogRay(int logType, string message)
+        {
+            var logLevel = logType switch
+            {
+                0 => ILogger.LogLevel.Trace,
+                _ => ILogger.LogLevel.Info,
+            };
+
+            switch (logLevel)
+            {
+                case ILogger.LogLevel.Trace:
+                    _logger.LogTrace(message);
+                    break;
+                case ILogger.LogLevel.Debug:
+                    _logger.LogDebug(message);
+                    break;
+                case ILogger.LogLevel.Info:
+                    _logger.LogInfo(message);
+                    break;
+                case ILogger.LogLevel.Warning:
+                    _logger.LogWarning(message); 
+                    break;    
+                case ILogger.LogLevel.Error:
+                    _logger.LogError(message); 
+                    break;
+            }
+        }
+    }
+}
