7bae17e0c9505f066486d977d1383641b6d17982
megabombus-rex <czechumennakompie@gmail.com>
2024-10-05 22:16:52 +0200

    Corrected exceptions to match pattern.

--- Exceptions/SceneNotFoundException.cs
+++ Exceptions/SceneNotFoundException.cs
@@ -1,7 +1,11 @@
 ﻿namespace LF2Clone.Exceptions
 {
+    [Serializable]
     public class SceneNotFoundException : Exception
     {
+        public SceneNotFoundException() { }
         public SceneNotFoundException(string message) : base(message) { }
+        public SceneNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
     }
 }
